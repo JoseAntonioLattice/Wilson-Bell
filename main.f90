39ed7505640fb3f30fe973f9dc5682a1af89216d
pepe <antonio.garcia.lattice@gmail.com>
2024-09-03 22:41:16 -0600

    2nd version not working

--- main.f90
+++ main.f90
@@ -3,38 +3,78 @@ program main
   implicit none
  
   real(dp), parameter :: pi = acos(-1.0_dp)
-  integer(i4), parameter :: d = 3
-  real(dp), dimension(d) :: q
-  integer(i4), dimension(d) :: n
-  integer(i4), parameter :: L = 10
-  integer(i4), dimension(d) :: l_vec
-  real(dp) :: s, s_prod,rho_star, omega_star,a, b, a_star, z
-  integer(i4) :: i,l1,l2,l3
-  
+  !real(dp) :: a, a_star, b, z
 
-  n = [1,1,1]
-  omega_star = 0.0_dp
-  do l1 = -L, L
-     do l2 = -L, L
-        do l3 = -L, L
-           l_vec = [l1,l2,l3]
-           q = 2*pi/L * n
-           do i = 1, 3
-              s_prod = 1.0_dp
-              s = (sin(q(i)/2)/(q(i)/2 + pi*l_vec(i)))**2
-              s_prod = s_prod*s
-           end do
-           omega_star = omega_star + s_prod/(norm2(q+2*pi*l_vec))**2
-        end do
-     end do
-  end do
-
-  z = 8.0_dp
-  a = 8.0_dp
-  b = 2**(-2.5_dp)
-  a_star = a*(1.0_dp - 2**d * b**2)
-  rho_star = a_star/(1.0_dp - (norm2(q))**2 * Omega_Star)
+  !z = 1.0_dp
+  !a = 8.0_dp
+  !b = 2**(-2.5_dp)
+  !a_star = a * (1.0_dp - 2**3 * b**2)
+  !print*, a_star, b
   
-  print*, omega_star
-  print*, rho_star
+  print*, fourier([1,1,1],10,6.0_dp,2**(2.5_dp),1.0_dp)
+contains
+
+  function omega_star(n,L)
+    real(dp) :: omega_star
+    integer(i4), dimension(3), intent(in) :: n
+    integer(i4), intent(in) :: L
+    integer(i4) :: i,l1,l2,l3
+    integer(i4), dimension(3) :: l_vec
+    real(dp) :: s_prod, q(3)
+
+    q = 2*pi*n/L
+    omega_star = 0.0_dp
+    do l1 = -L, L
+       do l2 = -L, L
+          do l3 = -L, L
+             l_vec = [l1,l2,l3]
+             do i = 1, 3
+                s_prod = 1.0_dp
+                if( n(i) == 0 .and. l_vec(i) == 0)then
+                   cycle
+                else
+                   s_prod = s_prod*(sin(q(i)/2)/(q(i)/2 + pi*l_vec(i)))**2
+                end if
+             end do
+             omega_star = omega_star + s_prod/(norm2(q + 2*pi*l_vec))**2
+          end do
+       end do
+    end do
+  end function omega_star
+
+  function rho_star(n,L,a_star,b,z)
+    real(dp) :: rho_star
+    real(dp), intent(in)  :: a_star,z,b
+    integer(i4), dimension(3), intent(in) :: n
+    integer(i4), intent(in) :: L
+
+    if( n(1) == 0 .and. n(2) == 0 .and. n(3) == 0 )then
+       rho_star = 0.0_dp
+    else
+       rho_star = a_star/(1.0_dp + (a_star/z) * omega_star(n,L))
+    end if
+    
+  end function rho_star
+
+  function fourier(r,L,a_star,b,z)
+    real(dp), intent(in)  :: a_star,z,b
+    integer(i4), intent(in) :: L
+    integer(i4) :: n(3),n1,n2,n3
+    real(dp) :: fourier, q(3)
+    integer(i4), dimension(3), intent(in) :: r
+
+    fourier = 0.0_dp
+    do n1 = 0, L - 1
+       do n2 = 0, L - 1
+          do n3 = 0, L - 1
+             n = [n1,n2,n3]
+             q = 2*pi/L * n
+             fourier = fourier + cos(dot_product(q,r)) * rho_star(n,L,a_star,b,z) 
+          end do
+       end do
+    end do
+    fourier = fourier / L**3
+  end function fourier
+
+  
 end program main
